a32aad6a4d626bca8987d01d5923f7b0547b96e8
andrew kim <mr.kimpa4i@gmail.com>
2022-05-27 13:14:12 +0300

    add workflow

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,8 +1,14 @@
+import { strict as assert } from 'assert';
 import capitalize from "../src/capitalize.js";
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
-  if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
-  console.log('Все тесты пройдены!');
+// if (capitalize('') !== '') {
+//     throw new Error('Функция работает неверно!');
+//   }
+//   if (capitalize('hello') !== 'Hello') {
+//     throw new Error('Функция работает неверно!');
+//   }
+//   console.log('Все тесты пройдены!');
+
+  assert.equal(capitalize(''), '');
+  assert.equal(capitalize('hello'), 'Hello');
+
+console.log('Все тесты пройдены!');
